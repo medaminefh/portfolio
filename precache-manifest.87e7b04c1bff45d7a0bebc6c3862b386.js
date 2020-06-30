5928d097becf3c5bf8bdf537bf30dc17f7f62c1d
med-amine-fh <fhal.mohamed.amine@gmail.com>
2020-06-30 14:09:33 +0100

    Updates

---
+++ precache-manifest.87e7b04c1bff45d7a0bebc6c3862b386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038be4e86980e95d80954fb4c070c12e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bcc8d592c4a6810c3437",
+    "url": "/portfolio/static/css/main.ecd899a1.chunk.css"
+  },
+  {
+    "revision": "faa1e46886993a25e43a",
+    "url": "/portfolio/static/js/2.f0f5f999.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f0f5f999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc8d592c4a6810c3437",
+    "url": "/portfolio/static/js/main.4d974b2c.chunk.js"
+  },
+  {
+    "revision": "e601d6e65f59a1ba94d6",
+    "url": "/portfolio/static/js/runtime-main.2b5d5641.js"
+  },
+  {
+    "revision": "56b5c7595f4f2ae338490c4d5347164f",
+    "url": "/portfolio/static/media/me2.56b5c759.jpg"
+  }
+]);
