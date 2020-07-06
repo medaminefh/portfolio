de173d452332648955fff4a198320363812afb00
med-amine-fh <fhal.mohamed.amine@gmail.com>
2020-07-06 10:05:44 +0100

    deleting console log in the nav

--- src/Components/Desktops/components/Nav/Nav.js
+++ src/Components/Desktops/components/Nav/Nav.js
@@ -1,14 +1,10 @@
-import React, { useRef } from "react";
+import React from "react";
 import classes from "./Nav.module.css";
 
 export const Nav = () => {
-  const nav = useRef("");
   const links = ["Home", "About", "Projects", "Contact"];
-  window.addEventListener("scroll", () => {
-    console.log(nav.current);
-  });
   return (
-    <nav ref={nav} className={classes.nav}>
+    <nav className={classes.nav}>
       <a href="#Home">
         <h2 className={classes.logo}>Portfolio</h2>
       </a>
