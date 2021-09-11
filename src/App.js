5990d4fc2fff5517a15df22fc54139f7b2f931d2
med-amine-fh <fhal.mohamed.amine@gmail.com>
2021-09-11 09:36:38 +0100

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { Contact, About, Nav, Header, Projects, Devices } from "./Components";
-import {
-  Switch,
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-} from "react-router-dom";
 
 function App() {
-  const [show, setShow] = useState("");
-
-  useEffect(() => {
-    window.outerWidth <= 1100 ? setShow(true) : setShow(false);
-    window.addEventListener("resize", (e) => {
-      e.target.outerWidth <= 1100 ? setShow(true) : setShow(false);
-    });
-  }, [show]);
   return (
-    <Router>
-      <div className="App">
-        {!show ? <Nav /> : <Devices />}
-        <Switch>
-          <Route exact path="/" render={(props) => <Header {...props} />} />
-          <Redirect from="/Home" to="/" />
-          <Route path="/Projects" render={(props) => <Projects {...props} />} />
-          <Route path="/About" render={(props) => <About {...props} />} />
-          <Route path="/Contact" render={(props) => <Contact {...props} />} />
-        </Switch>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div id="content">
+              <h1>Coming Soon!</h1>
+            </div>
+          </div>
+        </div>
       </div>
-    </Router>
+    </>
   );
 }
 
--- src/Components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Nav } from "./Nav/Nav";
-export { default as Devices } from "./Nav/devices";
-export { default as Header } from "./Header/Header";
-export { default as Projects } from "./Projects/Projects";
-export { default as About } from "./About/About";
-export { default as Contact } from "./Contact/Contact";
