e4b7b053542001e8ad2bf9a88ea648a145a18243
med-amine-fh <fhal.mohamed.amine@gmail.com>
2021-09-26 07:50:21 +0100

    add social media links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SvgBg from "./Background/backgroundAnimation"
 import "./App.css";
 
 function App() {
   return (
     <>
+    <SvgBg />
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -13,6 +15,38 @@ function App() {
           </div>
         </div>
       </div>
+      <ul className="social__media">
+      <li>
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://github.com/medaminefh"
+          
+        >
+          <i className="fa fa-github"></i>
+        </a>
+      </li>
+      <li>
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://twitter.com/medaminefh"
+          
+        >
+          <i className="fa fa-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://www.linkedin.com/in/mohamed-amine-fhal"
+          
+        >
+          <i className="fa fa-linkedin"></i>
+        </a>
+      </li>
+    </ul>
     </>
   );
 }
